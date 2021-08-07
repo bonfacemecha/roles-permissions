64f88650e60121d8dccdc40f1cc5fa279427c06d
Bonface Angwenyi <Bonface.Angwenyi@jubileekenya.com>
2021-08-07 23:44:17 +0300

    Initial commit

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        if (Auth::user()->hasRole('user')) {
+            return view('userdashboard');
+        } elseif (Auth::user()->hasRole('blogwriter')) {
+            return view('writerdashboard');
+        } elseif (Auth::user()->hasRole('admin')) {
+            return view('dashboard');
+        }
+    }
+    public function myprofile () {
+        return view('myprofile');
+    }
+}
